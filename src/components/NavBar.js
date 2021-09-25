00983e912a223dc109af91042f8a0ecd29353a42
RodriBlanca <rodrigoblanca96@gmail.com>
2021-09-25 15:34:24 -0300

    desafío clase 3 y 4

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <fragment>
+            <h1 className='text-danger'>E-commerce</h1>
+            <nav className='navbar'>
+                <p className='navbar-item'>Sobre Nosotros</p>
+                <p className='navbar-item'>Producto</p>
+                <p className='navbar-item'>Contacto</p>
+            </nav>
+        </fragment>
+    )
+}
+
+export default NavBar
