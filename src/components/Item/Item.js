fd2d3b72a4a69d80c877c6617f2207bf5412710c
RodriBlanca <rodrigoblanca96@gmail.com>
2021-10-08 11:05:39 -0300

    desafío 3

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,7 +5,7 @@ const Item = ({city, price, img}) => {
     return(
         <div className="item">
             <div className="item-img">
-                <img src={img} alt={city}></img>
+                <img className="item-img__picture" src={img} alt={city}></img>
             </div>
             <div className="item-information">
                 <h2 className="item-information__city">{city}</h2>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import Item from '../Item/Item';
 import './itemList.css';
+import barcelona from '../../assets/barcelona.jpg';
+import madrid from '../../assets/madrid.jpeg';
+import paris from '../../assets/paris.jpg';
+import londres from '../../assets/londres.jpg';
+import berlin from '../../assets/berlin.jpg';
 
 const ItemList = () => {
 
     const products = [
-        {city: 'Barcelona', price: 110000, id: 1, img: '../../../public/assets/img/barcelona.jpg'},
-        {city: 'Madrid', price: 100000, id: 2, img: '../../../public/assets/img/madrid.jpg'},
-        {city: 'Londres', price: 105000, id: 3, img: '../../../public/assets/img/londres.jpg'},
-        {city: 'Berlin', price: 90000, id: 4, img: '../../../public/assets/img/berlin.jpg'},
-        {city: 'Paris', price: 95000, id: 5, img: '../../../public/assets/img/paris.jpg'}
+        {city: 'Barcelona', price: 110000, id: 1, img: barcelona},
+        {city: 'Madrid', price: 100000, id: 2, img: madrid},
+        {city: 'Londres', price: 105000, id: 3, img: londres},
+        {city: 'Berlin', price: 90000, id: 4, img: berlin},
+        {city: 'Paris', price: 95000, id: 5, img: paris}
     ];
 
     return(
         <>
             <Item city={products[0].city} price={products[0].price} img={products[0].img}></Item>
-            <Item city={products[1].city} price={products[1].price}></Item>
-            <Item city={products[2].city} price={products[2].price}></Item>
+            <Item city={products[1].city} price={products[1].price} img={products[1].img}></Item>
+            <Item city={products[2].city} price={products[2].price} img={products[2].img}></Item>
         </>
     );
 }
